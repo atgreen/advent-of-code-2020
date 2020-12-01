2cf4450369f54d6da8d87807a8dd2decb8eafdce
Anthony Green <green@moxielogic.com>
2020-12-01 05:17:28 -0500

    Clean up

--- 01.lisp
+++ 01.lisp
@@ -2,19 +2,14 @@
 
 (ql:quickload :alexandria)
 
-(let ((number-list nil))
+(defparameter +numbers+ (mapcar #'parse-integer (uiop:read-file-lines "01.input")))
 
-  ;; Load all of the data...
-  (with-open-file (in "01.input" :direction :input)
-    (loop for line = (read-line in nil)
-          while line do (setf number-list (cons (parse-integer line) number-list))))
-
-  ;; Solve for steps 1 and 2 using the same mechanism...
-  (macrolet ((find-numbers (l)
-               `(block try-block
-                  (flet ((test-fn (p)
-                           (when (equal 2020 (apply #'+ p))
-                             (return-from try-block (apply #'* p)))))
-                    (alexandria:map-combinations #'test-fn number-list :length ,l)))))
-    (print (find-numbers 2))
-    (print (find-numbers 3))))
+;; Solve for steps 1 and 2 using the same mechanism...
+(macrolet ((find-numbers (l)
+             `(block try-block
+                (flet ((test-fn (p)
+                         (when (equal 2020 (apply #'+ p))
+                           (return-from try-block (apply #'* p)))))
+                  (alexandria:map-combinations #'test-fn +numbers+ :length ,l)))))
+  (print (find-numbers 2))
+  (print (find-numbers 3)))
