44b0d73199fcba5905ef629cfad60b7695145cb2
Anthony Green <green@moxielogic.com>
2020-12-06 08:35:29 -0500

    Simplify with reduce

--- 06.lisp
+++ 06.lisp
@@ -16,16 +16,10 @@
 (apply '+
        (mapcar
         (lambda (group)
-          (length
-           (loop for answer in (cdr group)
-                 with ilist = (car group)
-                 do (setf ilist (intersection ilist answer))
-                 finally (return ilist))))
-        (mapcar
-         (lambda (group)
-           (mapcar
-            (lambda (answer)
-              (coerce answer 'list))
-            (cl-ppcre:split "\\n" group)))
-         (cl-ppcre:split "\\n\\n"
-                         (alexandria:read-file-into-string "06.input")))))
+          (length (reduce #'intersection group)))
+        (mapcar (lambda (group)
+                  (mapcar (lambda (answer)
+                            (coerce answer 'list))
+                          (cl-ppcre:split "\\n" group)))
+                (cl-ppcre:split "\\n\\n"
+                                (alexandria:read-file-into-string "06.input")))))
